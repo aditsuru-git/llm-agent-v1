d914a1194488db45fe139af0b616ce23a8a296a8
aditsuru <ripedown1@gmail.com>
2025-03-02 11:53:26 +0530

    bug fixes

--- src/utils/validate.js
+++ src/utils/validate.js
@@ -1,11 +1,11 @@
 import path from "path";
 import { playGroundPath } from "../config/configPath.js";
-
+const normalizedPlayGroundPath = path.normalize(playGroundPath);
 function isInPlayground(targetPath) {
   const resolvedTargetPath = path.resolve(targetPath);
   return (
-    resolvedTargetPath === playGroundPath ||
-    resolvedTargetPath.startsWith(playGroundPath + path.sep)
+    resolvedTargetPath === normalizedPlayGroundPath ||
+    resolvedTargetPath.startsWith(normalizedPlayGroundPath + path.sep)
   );
 }
 
